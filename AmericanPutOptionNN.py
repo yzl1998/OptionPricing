a4471360b2bd002f79e78a52d8eea61efb2042db
yangzhonglin <zhonglinyang98@gmail.com>
2021-02-09 00:07:13 -0500

    Put Option NN Commit

--- AmericanPutOptionNN.py
+++ AmericanPutOptionNN.py
@@ -2,7 +2,6 @@ import math
 import numpy as np
 from time import time
 import tensorflow as tf
-from tensorflow.keras.datasets import boston_housing
 
 # legendre polynomial functions
 def l_0(val_arg):
@@ -37,15 +36,6 @@ def l_6(val_arg):
                                      - 1 / 20 * (val_arg ** 5) + 1 / 720 * (val_arg ** 6))
 
 
-# check whether to exercise or continue
-def whether_exercise(S, K, k, coefficients, test_functions):
-    continue_val = 0
-    n = len(test_functions)
-    for i in range(0, n):
-        continue_val += coefficients[k][i] * test_functions[i](S)
-    return max(K - S, 0.0) > continue_val
-
-
 def smooth_digit(S, K):
     return -1.0 / (1.0 + math.exp(-(K - S) / 0.005))
 
@@ -55,10 +45,11 @@ def smooth_diac(S, K):
 
 
 # compare the continuation value and the current payoff
-def determine(continue_val, exercise_val, cashflow, m):
+def determine(continue_val, exercise_val, cashflow, m, j, exercise_boundary):
     for i in range(0, m):
         if continue_val[i] < exercise_val[i]:
             cashflow[i] = exercise_val[i]
+            exercise_boundary[i] = j
 
 
 # evaluate the payoff
@@ -67,108 +58,57 @@ def payoff(exercise_val, path, i, K, m):
         exercise_val[j] = max(0.0, K - path[j][i])
 
 
-# regression function
-def regression(x_regression, y_regression, size_regression, coefficients, k, test_functions):
-    size_1 = size_regression
-    size_2 = len(test_functions)
-    y_regression_2 = np.zeros(size_regression)
-
-    # construct the matrix phi
-    d_phi = np.zeros((size_1, size_2))
-    for i in range(0, size_1):
-        y_regression_2[i] = y_regression[i]
-        for j in range(0, size_2):
-            d_phi[i][j] = test_functions[j](x_regression[i])
-
-"""
-# construct A = phi^T * phi, b = phi^T * Y
-    d_phi_transpose = d_phi.transpose()
-    A = np.dot(d_phi_transpose, d_phi)
-    b = np.dot(d_phi_transpose, y_regression_2)
-    # solve linear equations Ax = b
-    try:
-        x = np.linalg.solve(A, b)
-        for i in range(0, size_2):
-            coefficients[k][i] = x[i]
-    except np.linalg.LinAlgError as err:
-        if 'Singular matrix' in str(err):
-            raise np.linalg.LinAlgError("Singular Matrix Error!")
-        else:
-            raise np.linalg.LinAlgError("Unexpected Error!")
-"""
-
-# evaluate the continuation value
-def projection(continue_val, coefficients, k, test_functions, path, m):
-    n = len(test_functions)
-    for i in range(0, m):
-        continue_val[i] = 0
-        for j in range(0, n):
-            continue_val[i] += coefficients[k][j] * test_functions[j](path[i][k])
-
-
-# generate the paths and do the regression steps backwards
-def euler_path(m, n, S0, sigma, r, K, T, dW, coefficients, test_functions):
-    path = np.zeros((m, n))
-    dt = T / n
+# generate paths
+def generate_paths(m, n, S0, sigma, r, dt, dW, path):
     S = S0
-
-    # generate all paths
     for j in range(0, m):
         for i in range(0, n):
             S += r * S * dt + sigma[i] * S * math.sqrt(dt) * dW[j][i]
             path[j][i] = S
         S = S0
 
-    # do the regression steps backwards
+# generate the paths and do the regression steps backwards
+def euler_path(m, n, S0, sigma, r, K, T, dW, test_functions, exercise_boundary):
+    path = np.zeros((m, n))
+    dt = T / n
+
+    generate_paths(m, n, S0, sigma, r, dt, dW, path)
+
     continue_val = np.zeros(m)
     exercise_val = np.zeros(m)
     cashflow = np.zeros(m)
 
     for i in range(n - 1, 0, -1):
-        projection(continue_val, coefficients, i, test_functions, path, m)
+        # EXERCISE VALUE[0 ... m] = MAX(0.0, K - PATH[0 ... m][FROM 98 TO 1])
         payoff(exercise_val, path, i, K, m)
-        determine(continue_val, exercise_val, cashflow, m)
+        # IF EXERCISE[0 ... m] > CONTINUATION[0 ... m] CASHFLOW[0 ... m] = EXERCISE[0 ... m]
+        determine(continue_val, exercise_val, cashflow, m, i, exercise_boundary)
 
-        x_regression = np.zeros(m)
-        y_regression = np.zeros(m)
+        x_train = np.zeros((m, len(test_functions)))
+        y_train = np.zeros(m)
         size_regression = 0
+
+        x = np.zeros((m, len(test_functions)))
+        y = np.zeros(m)
+
+        print("i")
+        print(i)
 
         for j in range(0, m):
             cashflow[j] *= math.exp(-r * dt)
+            for n in range(0, len(test_functions)):
+                x[j][n] = test_functions[n](path[j][i - 1])
+            y[j] = cashflow[j]
             if path[j][i] < K:
-                x_regression[size_regression] = path[j][i - 1]
-                y_regression[size_regression] = cashflow[j]
+                for n in range(0, len(test_functions)):
+                    x_train[size_regression][n] = test_functions[n](path[j][i-1])
+                y_train[size_regression] = cashflow[j]
                 size_regression += 1
 
-        print("i & size_regression ")
-        print(i)
-        print(size_regression)
-        """
-        regression(x_regression, y_regression, size_regression, coefficients, i - 1, test_functions)
-        """
+        x_train = x_train[:size_regression]
+        y_train = y_train[:size_regression]
 
-        # construct the matrix phi
-
-        d_phi = np.zeros((size_regression, len(test_functions)))
-        y_regression_2 = np.zeros(size_regression)
-        for m in range(0, size_regression):
-            y_regression_2[m] = y_regression[m]
-            for n in range(0, len(test_functions)):
-                d_phi[m][n] = test_functions[n](x_regression[m])
-
-        x_train = d_phi
-        print("actual")
-        print(y_regression_2)
-        """
-        print(len(x_train))
-        print(x_train)
-        """
-        y_train = y_regression_2
-        """
-        print(len(y_train))
-        print(y_train)
-        """
-
+        # NN
         mean_label = y_train.mean(axis=0)
         std_label = y_train.std(axis=0)
         mean_feat = x_train.mean(axis=0)
@@ -178,64 +118,61 @@ def euler_path(m, n, S0, sigma, r, K, T, dW, coefficients, test_functions):
 
         linear_model = SimpleLinearRegression('zeros')
         linear_model.train(x_train, y_train, learning_rate=0.1, epochs=50)
-        pred = linear_model.predict(x_train)
-        pred *= std_label
-        pred += mean_label
 
-        print("predict")
-        print(pred)
+        """
+        mean_label = y.mean(axis=0)
+        std_label = y.std(axis=0)
+        mean_feat = x.mean(axis=0)
+        std_feat = x.std(axis=0)
+        """
 
+        x = (x-mean_feat)/std_feat
+        continue_val = linear_model.predict(x)
+        continue_val *= std_label
+        continue_val += mean_label
+
+        """
         sum = 0.0
-        for k in range(0, size_regression):
-            sum += (pred[k] - y_regression_2[k])**2
-            print("sum += ")
-            print((pred[k] - y_regression_2[k])**2)
+        for k in range(0, m):
+            sum += (continue_val[k] - y[k])**2
         print("squares sum:")
         print(sum)
-        # print(linear_model.m.numpy)
-        coefficients[i] = linear_model.m.read_value()
+        """
 
-        print(coefficients[99])
-        print(coefficients[101])
-
-
-    projection(continue_val, coefficients, 0, test_functions, path, m)
     payoff(exercise_val, path, 0, K, m)
-    determine(continue_val, exercise_val, cashflow, m)
-    print(continue_val)
-    V = 0
+    determine(continue_val, exercise_val, cashflow, m, 0, exercise_boundary)
 
-    # evaluate the option
+    V = 0
     for j in range(0, m):
         V += math.exp(-r * dt) * cashflow[j]
-    return V
+    return V / m
 
 
 class AmericanPutOption(object):
 
-    def __init__(self, m, n):
+    def __init__(self, m, n, S0, T, K, r, sigma):
         self.m = m
         self.n = n
-        self.S0 = 1
-        self.T = 1
-        self.K = 1
-        self.r = 0.04
-        self.sigma = np.full(n, 0.2)
+        self.S0 = S0
+        self.T = T
+        self.K = K
+        self.r = r
+        self.sigma = np.full(n, sigma)
         self.vega = np.full(n, 0)
         self.vanna = np.full(n, 0)
         self.dW = np.zeros((m, n))
-        self.S = self.S0
+        self.S = S0
         self.delta = 1
-        self.gamma = 0
         self.vegaBS = 0
+        self.gamma = 0
         self.vannaBS = 0
         self.V = 0
 
     # the pricing function using tangent method
-    def tangent_method(self, coefficients, test_functions):
+    def tangent_method(self, test_functions, exercise_boundary):
         sum, sumt, sumd, sumtt, sumtd = 0, 0, 0, 0, 0
         S0 = self.S
-        delta0, vegaBS0, gamma0, vannaBS0= self.delta, self.vegaBS, self.gamma, self.vannaBS
+        delta0, vegaBS0, gamma0, vannaBS0= 1, 0, 0, 0
         dt = self.T/self.n
 
         for j in range(0, self.m):
@@ -248,7 +185,7 @@ class AmericanPutOption(object):
                 self.delta += self.r*self.delta*dt + self.sigma[i]*self.delta*math.sqrt(dt)*self.dW[j][i]
                 self.S += self.r*self.S*dt + self.sigma[i]*self.S*math.sqrt(dt)*self.dW[j][i]
                 t = dt*(i+1)
-                if whether_exercise(self.S, self.K, i, coefficients, test_functions):
+                if exercise_boundary[j] == i:
                     sum += max(self.K - self.S, 0.0)*math.exp(- self.r*t)
                     sumt += smooth_digit(self.S, self.K)*math.exp(- self.r*t)*self.delta
                     sumd += smooth_digit(self.S, self.K)*math.exp(- self.r*t)*self.vegaBS
@@ -270,37 +207,32 @@ class AmericanPutOption(object):
         self.vannaBS = sumtd/self.m
 
 
-def main(m, n):
-    ao = AmericanPutOption(m, n)
+def main(m, n, S0, T, K, r, sigma, seed_1, seed_2):
+    ao = AmericanPutOption(m, n, S0, T, K, r, sigma)
 
-    np.random.seed(4)
+    np.random.seed(seed_1)
     ao.dW = np.random.normal(0,1,(m,n))
 
     test_functions = [l_0, l_1, l_2, l_3, l_4, l_5, l_6]
-    coefficients = np.full((n, len(test_functions)), 0.0)
+    exercise_boundary = np.full(m, 100)
 
-    # generate the paths and calculate the regression coefficients
-    ao.V = euler_path(m, n, ao.S0, ao.sigma, ao.r, ao.K, ao.T, ao.dW, coefficients, test_functions)
+    # generate the paths and calculate the exercise boundaries
+    ao.V = euler_path(m, n, ao.S0, ao.sigma, ao.r, ao.K, ao.T, ao.dW, test_functions, exercise_boundary)
 
-    # print(coefficients)
     print("V = {}".format(ao.V))
 
-    np.random.seed(2)
-    ao = AmericanPutOption(2*m, n)
-    ao.dW = np.random.normal(0,1,(2*m,n))
+    np.random.seed(seed_2)
+    ao = AmericanPutOption(m, n, S0, T, K, r, sigma)
+    ao.dW = np.random.normal(0,1,(m,n))
 
     # calculate the price of the American option
-    ao.tangent_method(coefficients,test_functions)
+    ao.tangent_method(test_functions, exercise_boundary)
 
     print("V_lower = {}".format(ao.S))
-    # dV/dS0
-    print("delta = {}".format(ao.delta))
-    # dV2/dS02
-    print("gamma = {}".format(ao.gamma))
-    # dV/dsigma
-    print("vegaBS = {}".format(ao.vegaBS))
-    # dV2/dsigma dS0
-    print("vannaBS = {}".format(ao.vannaBS))
+    print("Delta = {}".format(ao.delta))
+    print("VegaBS = {}".format(ao.vegaBS))
+    print("Gamma = {}".format(ao.gamma))
+    print("VannaBS = {}".format(ao.vannaBS))
 
 class SimpleLinearRegression:
     def __init__(self, initializer='random'):
@@ -324,8 +256,7 @@ class SimpleLinearRegression:
         with tf.GradientTape(persistent=True) as g:
             loss = self.mse(y, self.predict(X))
 
-        print("Loss: ", loss)
-
+        # print("Loss: ", loss)
         dy_dm = g.gradient(loss, self.m)
         dy_db = g.gradient(loss, self.b)
 
@@ -340,13 +271,12 @@ class SimpleLinearRegression:
         self.m.assign([self.var]*X.shape[-1])
 
         for i in range(epochs):
-            """
-            print("Epoch: ", i)
-            """
+            # print("Epoch: ", i)
             self.update(X, y, learning_rate)
 
 t0 = time()
-main(10000, 100)
+#      m    n   S0  T  K    r  sigma seed_1 seed_2
+main(10000, 100, 1, 1, 1, 0.04, 0.2, 4, 4)
 t1 = time()
 d1 = t1 - t0
 print ("Duration in Seconds %6.3f" % d1)
